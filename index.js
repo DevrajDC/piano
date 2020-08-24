4c51e72338d7dc554265af95672a3187b36cbd6d
adyasha9 <42711369+adyasha9@users.noreply.github.com>
2020-08-24 14:34:01 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,102 @@
+document.addEventListener("keydown", function(event) {
+    if (event.key==="A") {
+        let audio = new Audio("C:\\Users\\adyasha\\Downloads\\white_keys\\A.mp3");
+        audio.play();
+
+    }
+    else if(event.key === "a") {
+        let audio = new Audio("C:\\Users\\adyasha\\Downloads\\white_keys\\A.mp3");
+        audio.play();
+    }
+    else if(event.key === "S") {
+        let audio = new Audio("C:\\Users\\adyasha\\Downloads\\white_keys\\S.mp3");
+        audio.play();
+    }
+    else if(event.key === "s") {
+        let audio = new Audio("C:\\Users\\adyasha\\Downloads\\white_keys\\S.mp3");
+        audio.play();
+    }
+    else if(event.key === "D") {
+        let audio = new Audio("C:\\Users\\adyasha\\Downloads\\white_keys\\D.mp3");
+        audio.play();
+    }
+    else if(event.key === "d") {
+        let audio = new Audio("C:\\Users\\adyasha\\Downloads\\white_keys\\D.mp3");
+        audio.play();
+    }
+    else if(event.key === "F") {
+        let audio = new Audio("C:\\Users\\adyasha\\Downloads\\white_keys\\F.mp3");
+        audio.play();
+    }
+    else if(event.key === "f") {
+        let audio = new Audio("C:\\Users\\adyasha\\Downloads\\white_keys\\F.mp3");
+        audio.play();
+    }
+    else if(event.key === "G") {
+        let audio = new Audio("C:\\Users\\adyasha\\Downloads\\white_keys\\G.mp3");
+        audio.play();
+    }
+    else if(event.key === "g") {
+        let audio = new Audio("C:\\Users\\adyasha\\Downloads\\white_keys\\G.mp3");
+        audio.play();
+    }
+    else if(event.key === "H") {
+        let audio = new Audio("C:\\Users\\adyasha\\Downloads\\white_keys\\H.mp3");
+        audio.play();
+    }
+    else if(event.key === "h") {
+        let audio = new Audio("C:\\Users\\adyasha\\Downloads\\white_keys\\H.mp3");
+        audio.play();
+    }
+    else if(event.key === "J") {
+        let audio = new Audio("C:\\Users\\adyasha\\Downloads\\white_keys\\J.mp3");
+        audio.play();
+    }
+    else if(event.key === "j") {
+        let audio = new Audio("C:\\Users\\adyasha\\Downloads\\white_keys\\J.mp3");
+        audio.play();
+    }
+    else if(event.key === "W") {
+        let audio = new Audio("C:\\Users\\adyasha\\Downloads\\black_keys\\W.mp3");
+        audio.play();
+    }
+    else if(event.key === "w") {
+        let audio = new Audio("C:\\Users\\adyasha\\Downloads\\black_keys\\W.mp3");
+        audio.play();
+    }
+    else if(event.key === "E") {
+        let audio = new Audio("C:\\Users\\adyasha\\Downloads\\black_keys\\E.mp3");
+        audio.play();
+    }
+    else if(event.key === "e") {
+        let audio = new Audio("C:\\Users\\adyasha\\Downloads\\black_keys\\E.mp3");
+        audio.play();
+    }
+    else if(event.key === "T") {
+        let audio = new Audio("C:\\Users\\adyasha\\Downloads\\black_keys\\T.mp3");
+        audio.play();
+    }
+    else if(event.key === "t") {
+        let audio = new Audio("C:\\Users\\adyasha\\Downloads\\black_keys\\T.mp3");
+        audio.play();
+    }
+    else if(event.key === "Y") {
+        let audio = new Audio("C:\\Users\\adyasha\\Downloads\\black_keys\\Y.mp3");
+        audio.play();
+    }
+    else if(event.key === "y") {
+        let audio = new Audio("C:\\Users\\adyasha\\Downloads\\black_keys\\Y.mp3");
+        audio.play();
+    }
+    else if(event.key === "U") {
+        let audio = new Audio("C:\\Users\\adyasha\\Downloads\\black_keys\\U.mp3");
+        audio.play();
+    }
+    else if(event.key === "u") {
+        let audio = new Audio("C:\\Users\\adyasha\\Downloads\\black_keys\\U.mp3");
+        audio.play();
+    }
+    else {
+       console.log("wrong key");
+    }
+});
