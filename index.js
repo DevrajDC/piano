6ef0f8181921c6c2597080389add0c285dcf0991
Abhijay Jain <abhijay007j@gmail.com>
2020-10-06 12:49:53 +0530

    Update index.js
    code is not working so I make it work

--- index.js
+++ index.js
@@ -1,102 +1,32 @@
-document.addEventListener("keydown", function(event) {
-    if (event.key==="A") {
-        let audio = new Audio("C:\\Users\\adyasha\\Downloads\\white_keys\\A.mp3");
-        audio.play();
+const WHITE_KEYS = ['z', 'x', 'c', 'v', 'b', 'n', 'm']
+const BLACK_KEYS = ['s', 'd', 'g', 'h', 'j']
 
-    }
-    else if(event.key === "a") {
-        let audio = new Audio("C:\\Users\\adyasha\\Downloads\\white_keys\\A.mp3");
-        audio.play();
-    }
-    else if(event.key === "S") {
-        let audio = new Audio("C:\\Users\\adyasha\\Downloads\\white_keys\\S.mp3");
-        audio.play();
-    }
-    else if(event.key === "s") {
-        let audio = new Audio("C:\\Users\\adyasha\\Downloads\\white_keys\\S.mp3");
-        audio.play();
-    }
-    else if(event.key === "D") {
-        let audio = new Audio("C:\\Users\\adyasha\\Downloads\\white_keys\\D.mp3");
-        audio.play();
-    }
-    else if(event.key === "d") {
-        let audio = new Audio("C:\\Users\\adyasha\\Downloads\\white_keys\\D.mp3");
-        audio.play();
-    }
-    else if(event.key === "F") {
-        let audio = new Audio("C:\\Users\\adyasha\\Downloads\\white_keys\\F.mp3");
-        audio.play();
-    }
-    else if(event.key === "f") {
-        let audio = new Audio("C:\\Users\\adyasha\\Downloads\\white_keys\\F.mp3");
-        audio.play();
-    }
-    else if(event.key === "G") {
-        let audio = new Audio("C:\\Users\\adyasha\\Downloads\\white_keys\\G.mp3");
-        audio.play();
-    }
-    else if(event.key === "g") {
-        let audio = new Audio("C:\\Users\\adyasha\\Downloads\\white_keys\\G.mp3");
-        audio.play();
-    }
-    else if(event.key === "H") {
-        let audio = new Audio("C:\\Users\\adyasha\\Downloads\\white_keys\\H.mp3");
-        audio.play();
-    }
-    else if(event.key === "h") {
-        let audio = new Audio("C:\\Users\\adyasha\\Downloads\\white_keys\\H.mp3");
-        audio.play();
-    }
-    else if(event.key === "J") {
-        let audio = new Audio("C:\\Users\\adyasha\\Downloads\\white_keys\\J.mp3");
-        audio.play();
-    }
-    else if(event.key === "j") {
-        let audio = new Audio("C:\\Users\\adyasha\\Downloads\\white_keys\\J.mp3");
-        audio.play();
-    }
-    else if(event.key === "W") {
-        let audio = new Audio("C:\\Users\\adyasha\\Downloads\\black_keys\\W.mp3");
-        audio.play();
-    }
-    else if(event.key === "w") {
-        let audio = new Audio("C:\\Users\\adyasha\\Downloads\\black_keys\\W.mp3");
-        audio.play();
-    }
-    else if(event.key === "E") {
-        let audio = new Audio("C:\\Users\\adyasha\\Downloads\\black_keys\\E.mp3");
-        audio.play();
-    }
-    else if(event.key === "e") {
-        let audio = new Audio("C:\\Users\\adyasha\\Downloads\\black_keys\\E.mp3");
-        audio.play();
-    }
-    else if(event.key === "T") {
-        let audio = new Audio("C:\\Users\\adyasha\\Downloads\\black_keys\\T.mp3");
-        audio.play();
-    }
-    else if(event.key === "t") {
-        let audio = new Audio("C:\\Users\\adyasha\\Downloads\\black_keys\\T.mp3");
-        audio.play();
-    }
-    else if(event.key === "Y") {
-        let audio = new Audio("C:\\Users\\adyasha\\Downloads\\black_keys\\Y.mp3");
-        audio.play();
-    }
-    else if(event.key === "y") {
-        let audio = new Audio("C:\\Users\\adyasha\\Downloads\\black_keys\\Y.mp3");
-        audio.play();
-    }
-    else if(event.key === "U") {
-        let audio = new Audio("C:\\Users\\adyasha\\Downloads\\black_keys\\U.mp3");
-        audio.play();
-    }
-    else if(event.key === "u") {
-        let audio = new Audio("C:\\Users\\adyasha\\Downloads\\black_keys\\U.mp3");
-        audio.play();
-    }
-    else {
-       console.log("wrong key");
-    }
-});
+const keys = document.querySelectorAll('.key')
+const whiteKeys = document.querySelectorAll('.key.white')
+const blackKeys = document.querySelectorAll('.key.black')
+
+
+keys.forEach(key => {
+    key.addEventListener('click', () => playNote(key))
+})
+
+document.addEventListener('keydown', e => {
+    if (e.repeat) return
+    const key = e.key
+    const whiteKeyIndex = WHITE_KEYS.indexOf(key)
+    const blackKeyIndex = BLACK_KEYS.indexOf(key)
+
+    if (whiteKeyIndex > -1) playNote(whiteKeys[whiteKeyIndex])
+    if (blackKeyIndex > -1) playNote(blackKeys[blackKeyIndex])
+
+})
+
+function playNote(key) {
+    const noteAudio = document.getElementById(key.dataset.note)
+    noteAudio.currentTime = 0
+    noteAudio.play()
+    key.classList.add('active')
+    noteAudio.addEventListener('ended', () => {
+    key.classList.remove('active')
+    })
+}
